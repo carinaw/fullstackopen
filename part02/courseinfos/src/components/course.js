f19a6617cf1f35b52e39bc47e9a140cdcf794c25
Carina Wetzlhütter <carina.wetzlhuetter@gmail.com>
2019-10-29 22:58:54 +0200

    Create course module and import it to index.js

---
+++ part02/courseinfos/src/components/course.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Course = ({courses}) => {
+  return (
+    <div>
+    <Title title="Web development" />
+    <Name name={courses[0].name} />
+    <Parts parts={courses[0].parts} />
+    <Total parts={courses[0].parts} />
+    <Name name={courses[1].name} />
+    <Parts parts={courses[1].parts} />
+    <Total parts={courses[1].parts} />
+    </div>
+  )
+}
+
+const Title = ({title}) => {
+  return <h1>{title}</h1>
+}
+
+const Name = ({name}) => {
+  return (
+  <h2>{name}</h2>
+  )
+}
+
+const Part = ({part}) => {
+  return (
+  <p>{part.name}, {part.exercises} Exercises</p>
+  )
+}
+
+const Parts = ({parts}) => {
+  const info = parts.map(part => <Part key={part.id} part={part} />)
+  return (
+    <div>{info}</div>
+  )
+}
+
+const Total = ({parts}) => {
+
+  const totalexercises = parts.reduce((sum, exercise) => sum + exercise.exercises, 0)
+    return (
+      <h4 key={parts.id}>This is the total: {totalexercises}</h4>
+    )
+}
+
+
+
+
+
+export default Course
--- part02/courseinfos/src/index.js
+++ part02/courseinfos/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import Course from './components/course'
 
 const App = () => {
   const courses = [
@@ -50,46 +50,9 @@ const App = () => {
 
   return (
     <div>
-        <Title title="Web development" />
-        <Name name={courses[0].name} />
-        <Parts parts={courses[0].parts} />
-        <Total parts={courses[0].parts} />
-        <Name name={courses[1].name} />
-        <Parts parts={courses[1].parts} />
-        <Total parts={courses[1].parts} />
+    <Course courses={courses} />
     </div>
   )
-}
-
-const Title = ({title}) => {
-  return <h1>{title}</h1>
-}
-
-const Name = ({name}) => {
-  return (
-  <h2>{name}</h2>
-  )
-}
-
-const Part = ({part}) => {
-  return (
-  <p>{part.name}, {part.exercises} Exercises</p>
-  )
-}
-
-const Parts = ({parts}) => {
-  const info = parts.map(part => <Part key={part.id} part={part} />)
-  return (
-    <div>{info}</div>
-  )
-}
-
-const Total = ({parts}) => {
-
-  const totalexercises = parts.reduce((sum, exercise) => sum + exercise.exercises, 0)
-    return (
-      <h4 key={parts.id}>This is the total: {totalexercises}</h4>
-    )
 }
 
 
