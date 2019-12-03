1dca2f7f6b7fce5212c0c51612b5a998e677c751
Carina Wetzlhütter <carina.wetzlhuetter@gmail.com>
2019-12-03 17:56:04 +0200

    Make alert work and add phone number field

--- part02/phonebook/src/App.js
+++ part02/phonebook/src/App.js
@@ -4,10 +4,14 @@ import { useState } from 'react';
 
 const App = ({person}) => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '0452345401'},
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
 // Whatever changes within the input field - it is set as a newName at that point.
   const handleNameChange = (event) => {
@@ -15,26 +19,45 @@ const App = ({person}) => {
     console.log(event.target.value);
   }
 
+  const handlePhoneNumber = (event) => {
+    setNewNumber(event.target.value)
+    console.log(event.target.value);
+  }
 
-
-  const handleaddName = (event) => {
+  const handleaddInfo = (event) => {
     event.preventDefault()
 
+  // const handleFilterChange = (event) => {
+  //     setNewFilter(event.target.value)
+  //     console.log(event.target.value);
+  //   }
 
-// Create an object called personObject where the value from the input field at time of submit click is stored.
-    const personObject = {
-      name: newName
-    }
+  // Create an object called personObject where the value from the input field at time of submit click is stored.
+      const personObject = {
+        name: newName,
+        number: newNumber
+      };
 
-  // Add the personObject object that was created when button was clicked to the array persons using concat.
-    setPersons(persons.concat(personObject))
 
-// Clear newName state (and therefore field again)
-    setNewName('')
+
+  // Use map for a reason I still need to comprehend and show alert if newName is already in the array.
+    const allNames = persons.map(person => person.name)
+     if (allNames.includes(newName, 0) === true) {
+     window.alert(`${newName} is already on the list!`) }
+     else {
+// Add the personObject object that was created when button was clicked to the array persons using concat.
+       setPersons(persons.concat(personObject));
+     }
+
+  // Clear newName state (and therefore field again)
+      setNewName('')
+      setNewNumber('')
   }
 
   const rows = () => persons.map(person =>
-      <Person key={person.name} person={person}/>)
+    <div>
+      <Person key={person.name} person={person}/>
+    </div>)
 
 
 
@@ -43,9 +66,12 @@ console.log('people', persons);
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={handleaddName}>
+      <form onSubmit={handleaddInfo}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handlePhoneNumber} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -59,11 +85,20 @@ console.log('people', persons);
   )
 }
 
-const Person = ({person}) => {
+const Person = ({person, number}) => {
   return (
-    <li>{person.name}</li>
+    <div>
+    <li>{person.name} {person.number}</li>
+
+    </div>
   )
 }
 
+// const Error = ({persons, newName}) => {
+//   if (persons.includes(newName))
+//     return (
+//     window.alert("Hello world!")
+//     )
+// }
 
 export default App
