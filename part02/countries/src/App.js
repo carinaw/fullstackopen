d498ac718aa0812f9528ff7148fd7d5d60b71497
Carina Wetzlhütter <carina.wetzlhuetter@gmail.com>
2020-01-11 18:43:27 +0100

    Get country list working with API

--- part02/countries/src/App.js
+++ part02/countries/src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
-function App() {
+
+const App = () => {
+  const [ countries, setCountries ] = useState([])
+  const [ filter, setFilter ] = useState('')
+
+
+  const hook = () => {
+    console.log('effect')
+    axios
+    .get('https://restcountries.eu/rest/v2/all')
+    .then(response => {
+      console.log('promise fulfilled');
+      setCountries(response.data)
+    })
+  }
+  useEffect(hook, [])
+
+  console.log('render', countries.length, 'countries')
+
+
+
+
+    const handleFilter = (event) => {
+      setFilter(event.target.value);
+}
+    const showFiltered = countries.filter(country => country.name.includes(filter));
+
+    const rows = () => showFiltered.map(country =>
+      <div>
+        <Country key={country.alpha2code} country={country}/>
+      </div>
+    )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Country data</h2>
+      <div>
+      find countries: <input value={filter} onChange={handleFilter}/>
+      </div>
+    <div>
+      Länderliste
+      {rows()}
     </div>
-  );
+    </div>
+  )
 }
 
-export default App;
+const Country = ({country}) => {
+  return (
+    <div>
+      <li key={country.id}>{country.name}</li>
+    </div>
+  )
+}
+
+export default App
