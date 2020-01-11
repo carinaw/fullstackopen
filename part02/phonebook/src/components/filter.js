60587780dfbde21ff544599e8af5a5d69fdf8807
Carina Wetzlhütter <carina.wetzlhuetter@gmail.com>
2020-01-11 16:42:55 +0100

    Install and add files needed for step 6

---
+++ part02/phonebook/src/components/filter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import { useState } from 'react';
+
+
+const showFiltered = persons.filter(person => person.name.includes(filter));
+
+const rows = () => showFiltered.map(person =>
+  <div>
+    <Person key={person.name} person={person}/>
+  </div>
+)
+
+
+filter by: <input value={filter} onChange={handleFilter}/>
+
+
+export default Filter
