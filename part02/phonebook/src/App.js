1ee8f3ef0e10430c301aee524ea103302b8e2066
Carina Wetzlhütter <carina.wetzlhuetter@gmail.com>
2020-02-02 13:38:27 +0200

    Save persons in db & extract backend communication to module

--- part02/phonebook/src/App.js
+++ part02/phonebook/src/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import './App.css';
-import { useState } from 'react';
+import React from 'react'
+import './App.css'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import personService from './services/persons'
+
 
 const App = ({person}) => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '0452345401'},
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
+
+useEffect(() => {
+  personService
+  .getAll()
+  .then(initialPersons => {
+    console.log('promise fulfilled');
+    setPersons(initialPersons)
+  })
+}, [])
+
+
+console.log('render', persons.length, 'persons')
 
 // Whatever changes within the input field - it is set as a newName at that point.
   const handleNameChange = (event) => {
@@ -29,34 +38,36 @@ const App = ({person}) => {
     event.preventDefault()
 
   // Create an object called personObject where the value from the input field at time of submit click is stored.
-      const personObject = {
-        name: newName,
-        number: newNumber
-      };
+  const personObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    personService
+      .create(personObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
+      })
 
   // Use map for a reason I still need to comprehend and show alert if newName is already in the array.
-    const allNames = persons.map(person => person.name)
-     if (allNames.includes(newName, 0) === true) {
-     window.alert(`${newName} is already on the list!`) }
-     else {
-// Add the personObject object that was created when button was clicked to the array persons using concat.
-       setPersons(persons.concat(personObject));
-  // Clear newName state (and therefore field again)
-      setNewName('')
-      setNewNumber('')
-    };
-};
-
+    // const allNames = persons.map(person => person.name)
+    //  if (allNames.includes(newName, 0) === true) {
+    //  window.alert(`${newName} is already on the list!`) }
+}
     const handleFilter = (event) => {
       setFilter(event.target.value);
-}
-    const showFiltered = persons.filter(person => person.name.includes(filter));
+    }
+
+    const showFiltered = persons.filter(person =>
+      person.name.includes(filter));
 
     const rows = () => showFiltered.map(person =>
       <div>
-        <Person key={person.name} person={person}/>
+        <Person key={person.id} person={person}/>
       </div>
-    )
+      )
 
 
 console.log('people', persons);
@@ -90,7 +101,7 @@ console.log('filter', filter);
 const Person = ({person, number}) => {
   return (
     <div>
-    <li>{person.name} {person.number}</li>
+    <li key={person.id}>{person.name} {person.number}</li>
 
     </div>
   )
