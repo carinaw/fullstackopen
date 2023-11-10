91a1ecd3234efd5784f1c686c95aba4c1ee67226
Carina Wetzlhütter <carina.wetzlhuetter@gmail.com>
2023-11-10 11:03:24 +0200

    Add 1.12 anecdotes step1

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const App = () => {
+	const anecdotes = [
+		"If it hurts, do it more often.",
+		"Adding manpower to a late software project makes it later!",
+		"The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+		"Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+		"Premature optimization is the root of all evil.",
+		"Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+		"Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+		"The only way to go fast, is to go well.",
+	];
+
+	const [selected, setSelected] = useState(0);
+
+	const random = Math.random();
+	console.log(random);
+	const totalArray = anecdotes.length;
+	console.log(totalArray);
+	const randomIndex = Math.floor(random * totalArray);
+	console.log(randomIndex);
+
+	const handleRandomClick = () => {
+		setSelected(randomIndex);
+		console.log("selected", selected);
+	};
+
+	return (
+		<div>
+			<div>{anecdotes[selected]}</div>
+			<button onClick={handleRandomClick}>new anecdote</button>
+		</div>
+	);
+};
+
+export default App;
