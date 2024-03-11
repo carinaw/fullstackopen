f256f8f7215ca60abd59ff8c6d87c7cc6a700b03
Carina Wetzlhütter <carina.wetzlhuetter@gmail.com>
2024-03-11 11:22:43 +0200

    Format using eslint

--- part7/routed-anecdotes/src/hooks/index.js
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -1,15 +1,15 @@
 import { useState } from "react";
 
 export const useField = (name) => {
-	const [value, setValue] = useState("");
+  const [value, setValue] = useState("");
 
-	const onChange = (event) => {
-		setValue(event.target.value);
-	};
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
 
-	const onReset = () => {
-		setValue("");
-	};
+  const onReset = () => {
+    setValue("");
+  };
 
-	return { name, value, onChange, onReset };
+  return { name, value, onChange, onReset };
 };
